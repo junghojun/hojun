69480cb7a3e0bb375875bc6737760634b7627524
Administrator <Administrator@1YDSVUHQ0BRMYCA>
2014-10-07 15:16:01 +0900

    create table with append to element id

--- hojun/JAjax_border/WebContent/JScript/Jscript.js
+++ hojun/JAjax_border/WebContent/JScript/Jscript.js
@@ -2,7 +2,27 @@
  *  create date : mon, october 6
  */
 (function(){
-	var Create_Jborder =(function(json_data){
+	//basic table sample
+	var Create_Jborder =(function(json_data,Element_id){
+		
+		var top="<table>" +
+				"<tr>" +
+				"<td>NO</td>" +
+				"<td>title</td>" +
+				"<td>content</td>" +
+				"<td>date<td>" +
+				"<td>oder</td>" +
+				"</tr>";
+		$(Element_id).appendTo(top);
+		for (var int = 0; int < json_data.length; int++) {
+			top=+"<tr>" +
+					"<td>"+json_data.number+"</td>" +
+					"<td>"+json_data.title.substring(1,20)+"</td>"
+					"<td>"+json_data.content.substring(1,30)+"<td>" +
+					"<td>"+json_data.date+"<td>" +
+					"<td>"+json_data.oder+"</td>";
+		}
+		
 		
 	});
 	var Jpaging = (function(count_data,Elementid){
